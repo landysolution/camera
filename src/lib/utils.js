001c4abd93f60601f119dc6afb414c33e6cb7d93
landy <landy.solution@gmail.com>
2026-06-15 14:21:54 +0800

    ads

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,4 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
-export const API_URL = "http://localhost:9000";
+// export const API_URL = "http://localhost:9000";
+export const API_URL = process.env.NEXT_PUBLIC_API
